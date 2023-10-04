0ee2d8476bd737f2227fbc42f59bf907ba5012cb
Leonardo de Paula Fernandes <leofernandes9@gmail.com>
2023-10-04 01:43:49 -0300

    Forma de autenticacao corrigida para obter usuario por id

--- src/intermediarios/autenticacao.js
+++ src/intermediarios/autenticacao.js
@@ -1,20 +1,31 @@
 const jwt = require('jsonwebtoken');
 const senhaSegura = require('../chavesecreta');
+const pool = require('../banco_de_dados/conexao');
 
-const verificarLogin = (req, res, next) => {
+const verificarLogin = async (req, res, next) => {
     try {
         const { authorization } = req.headers
         if (!authorization) {
             return res.status(400).json({
-                mensagem: 'Token ausente no cabeçalho da requisição'
+                mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.'
             });
         }
 
         const token = authorization.split(' ')[1];
         const assinatura = jwt.verify(token, senhaSegura);
 
-        const { iat, exp, ...usuario } = assinatura;
-        req.usuario = usuario;
+        const { id } = assinatura;
+
+        const { rows, rowCount } = await pool.query(
+            'select * from usuarios where id = $1',
+            [id]
+        );
+
+        if (rowCount < 1) {
+            return res.status(401).json({ mensagem: 'Você não possui autorização para acessar esse recurso.' });
+        }
+
+        req.usuario = rows[0];
 
         next();
     } catch (error) {
